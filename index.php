3a231624afd964c0d59643ea5db0075ac6d2ce48
Léo Maisonneuve <leo.maisonneuve@gmail.com>
2016-06-08 00:55:26 +0200

    Presentation web-site

--- index.php
+++ index.php
@@ -1,15 +1,108 @@
 <!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <title>Title</title>
-</head>
-<body>
-<section>
-    <p>
-    <h1>UTC Now</h1>
-    Site de presentation d'UTC Now. Web developers, a vos claviers!
-    </p>
-</section>
-</body>
+<html>
+	<head>
+		<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+		<title>nf28</title>
+		<link rel="stylesheet" href="css/style.css" type="text/css" />
+	</head>
+	<body>
+		<div class="page">
+			<div class="header">
+				<a href="nf28.html" id="logo"><h1>UTC Now</h1></a>
+				<ul>
+					<li class="selected"><a href="nf28.html">Home</a></li>
+				</ul>
+			</div>
+			<div class="body">
+				<div id="featured">
+					<h3>UTC Now</h3>
+					<p>UTC Now, est une application mobile qui centralise tous les évènements et activités qui ont lieu à l’UTC. Il s’agit d’un agenda partagé et mis à jour en temps réel par l’ensemble des individus impliqués dans l’UTC, avec des vues personnalisées en fonction des préférences de l’utilisateur. Il est possible de recevoir des notifications correspondant à ses points d’intérêt.<br><br>
+					L’application regroupe des informations telles que:<br>
+						- Les événements associatifs<br>
+						- Les réunions d’information et présentations administratives<br>
+						- Les cours par salle et étudiants qui s’y trouvent<br>
+						- Calendrier scolaire, alternance des semaines, etc...</p>
+				</div>
+				<div id="inter">
+				<h3>Interfaces principaux</h3>
+				</div>
+				<ul class="blog">
+
+					<li>
+						<div>
+							<img src="images/image1.png" alt=""/>
+							<p>Nous utilisons une timeline horizontale (H­ à gauche et H+ a droite) qui représente une journée.Avec cet affichage, les évènements simultanés sont affichés les uns sous les autres. </p>
+
+						</div>
+					</li>
+					<li>
+						<div>
+							<img src="images/image2.png" alt=""/>
+							<p>Un champ de recherche situé en haut de l’interface permet aux utilisateurs d’effectuer des recherches dans la base de données. </p>
+
+						</div>
+					</li>
+					<li>
+						<div>
+							<img src="images/image3.png" alt=""/>
+							<p> Le filtrage est mis en place par le biais de checkbox qui associent chaque couleur à un type d’évènement. L’utilisateur coche ou décoche les cases pour afficher ou non le calendrier correspondant.</p>
+						</div>
+					</li>
+
+
+				</ul>
+				<ul class="blog">
+					<li>
+						<div>
+						<img src="images/image4.png" alt=""/>
+							<p>Le détail de l’évènement est affiché lorsque l’utilisateur clique sur celui­ci. L’utilisateur peut également indiquer s’il participe ou non à cet évènement.La liste affiche en priorité les participants susceptibles d’intéresser l’utilisateur.</p>
+
+						</div>
+					</li>
+					<li>
+						<div>
+							<img src="images/image5.png" alt=""/>
+							<p>L'utilisateur peut créer un évenement en précisant le nom,le lieu,la date,l'horaire ainsi que la description.Il peut aussi choisir de publier cet évenement à tout le monde.</p>
+												</div>
+					</li>
+					<li>
+						<div>
+							<img src="images/image6.png" alt=""/>
+							<p>L'utilisateur peut aussi voir le calendrier de ses amis.</p>
+
+						</div>
+					</li>
+
+				</ul>
+				<div id="others">
+					<h3>Technologies utilisées</h3>
+					<div class="ionic">
+
+
+						<p>Free and open source, Ionic offers a library of mobile-optimized HTML, CSS and JS components and tools for building highly interactive native and progressive web apps. Built with Sass, optimized for AngularJS.</p>
+					</div>
+					<div class="nodejs">
+
+
+						<p>Node.js® is a JavaScript runtime built on Chrome's V8 JavaScript engine. Node.js uses an event-driven, non-blocking I/O model that makes it lightweight and efficient. Node.js' package ecosystem, npm, is the largest ecosystem of open source libraries in the world.</p>
+					</div>
+
+				</div>
+			</div>
+			<div class="footer">
+				<ul>
+					<li><a href="nf28.html">Home</a></li>
+
+				</ul>
+				<p>Alexandre Abrantes,Baptiste Montangé,Théo Delalande­Delarbre,Xuner Huang,Léo Maisonneuve</p>
+				<p>2016 Projet NF28 </p><br><br><br>
+
+				<div class="connect ">
+					<a href="http://facebook.com/freewebsitetemplates" id="facebook">facebook</a>
+					<a href="http://twitter.com/fwtemplates" id="twitter">twitter</a>
+					<a href="http://www.youtube.com/fwtemplates" id="vimeo">vimeo</a>
+				</div>
+			</div>
+		</div>
+	</body>
 </html>
